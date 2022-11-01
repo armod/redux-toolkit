61b9c8499aa698e795fd25a3c1603882dbbc420b
armod <armods@gmail.com>
2022-11-01 19:04:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import CartContainer from './components/CartContainer'
 import { useDispatch, useSelector } from 'react-redux'
 import { calculateTotal } from './features/cart/cartSlice'
 import { useEffect } from 'react'
+import Modal from './components/Modal'
+
 function App() {
   const { cartItems } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -12,6 +15,7 @@ function App() {
   }, [cartItems])
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
